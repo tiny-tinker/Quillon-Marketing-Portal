0a1df8f2ff5c3d6a31ae89f9787f8c72a629d878
Han <jupiterhcg@gmail.com>
2018-04-29 19:29:50 -0700

    test 2018/4/30

--- src/scenes/Home/Home.js
+++ src/scenes/Home/Home.js
@@ -1,20 +1,24 @@
-import React from 'react'
-import { push } from 'react-router-redux'
-import { bindActionCreators } from 'redux'
-import { connect } from 'react-redux'
+import React from 'react';
+import { push } from 'react-router-redux';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
 
 const Home = props => (
-    <div>
-        <h1>Home</h1>
-        <p>Welcome home!</p>
+  <div>
+    <h1>Home</h1>
+    <p>Welcome home!</p>
+    <div className="container">
+      <h1> This is test </h1>
     </div>
-)
+  </div>
+);
 
-const mapDispatchToProps = dispatch => bindActionCreators({
-    changePage: () => push('/about-us')
-}, dispatch)
+const mapDispatchToProps = dispatch =>
+  bindActionCreators(
+    {
+      changePage: () => push('/about-us')
+    },
+    dispatch
+  );
 
-export default connect(
-    null,
-    mapDispatchToProps
-)(Home)
+export default connect(null, mapDispatchToProps)(Home);
