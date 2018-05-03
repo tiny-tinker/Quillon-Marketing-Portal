df7ac2f86efae95c0291eed1fdd56241df00a7a5
Han <jupiterhcg@gmail.com>
2018-05-03 00:00:09 -0700

    footer section 2018/5/3

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { Provider } from 'react-redux'
-import { ConnectedRouter } from 'react-router-redux'
-import store, { history } from './store'
+import { Provider } from 'react-redux';
+import { ConnectedRouter } from 'react-router-redux';
+import store, { history } from './store';
 import App from './scenes/app';
 import registerServiceWorker from './registerServiceWorker';
 import 'bootstrap/dist/css/bootstrap.css';
@@ -11,13 +11,13 @@ import './global.scss';
 const target = document.querySelector('#root');
 
 render(
-    <Provider store={store}>
-        <ConnectedRouter history={history}>
-            <div>
-                <App />
-            </div>
-        </ConnectedRouter>
-    </Provider>,
-    target
+  <Provider store={store}>
+    <ConnectedRouter history={history}>
+      <div>
+        <App />
+      </div>
+    </ConnectedRouter>
+  </Provider>,
+  target
 );
 registerServiceWorker();
--- src/scenes/Home/components/Footer/Footer.js
+++ src/scenes/Home/components/Footer/Footer.js
@@ -1,54 +1,72 @@
 import React, { Component } from 'react';
 
-//import './footer.scss';
+import FaFacebook from 'react-icons/lib/fa/facebook';
+import FaInstagram from 'react-icons/lib/fa/instagram';
+import FaTwitter from 'react-icons/lib/fa/twitter';
+import FaLinkedin from 'react-icons/lib/fa/linkedin';
+import FaGithub from 'react-icons/lib/fa/github';
+import FaSlack from 'react-icons/lib/fa/slack';
+import FaMedium from 'react-icons/lib/fa/medium';
+
+import './footer.css';
 
 export default class Footer extends Component {
   render() {
     return (
-      <div className="container">
-        <div>FOLLOW US</div>
-        <div className="row">
-          <div className="col-1">
-            <a href="https://www.facebook.com/quilloncurrency/" target="blank">
-              {' '}
-              <span className="glyphicon glyphicon-bell" />{' '}
-            </a>
-          </div>
-          <div className="col-1">
-            <a href="https://www.instagram.com/quilloncurrency/" target="blank">
-              {' '}
-              <i className="fa fa-instagram wow fadeInUp"> </i>{' '}
-            </a>
-          </div>
-          <div className="col-1">
-            <a href="https://twitter.com/QuillonCurrency" target="blank">
-              {' '}
-              <i className="fa fa-twitter wow fadeInUp"> </i>{' '}
-            </a>
-          </div>
-          <div className="col-1">
-            <a href="https://github.com/QuillonCurrency" target="blank">
-              {' '}
-              <i className="fa fa-github wow fadeInUp"> </i>{' '}
-            </a>
-          </div>
-          <div className="col-1">
-            <a href="https://www.linkedin.com/company/quillonfoundation/">
-              {' '}
-              <i className="fa fa-linkedin-square wow fadeInUp"> </i>{' '}
-            </a>
-          </div>
-          <div className="col-1">
-            <a href="https://quilloncurrency.slack.com" target="blank">
-              {' '}
-              <i className="fa fa-slack wow fadeInUp"> </i>{' '}
-            </a>
-          </div>
-          <div className="col-1">
-            <a href="https://medium.com/quillon" target="blank">
-              {' '}
-              <i className="fa fa-medium wow fadeInUp"> </i>{' '}
-            </a>
+      <div className="footer">
+        <div className="container text-center">
+          <div className="title text-white">FOLLOW US</div>
+          <div className="row">
+            <div className="col-lg-4"> </div>
+            <div className="col-12 col-lg-4">
+              <div className="row seven-cols">
+                <div className="col-1">
+                  <a
+                    href="https://www.facebook.com/quilloncurrency/"
+                    target="blank"
+                  >
+                    <FaFacebook color="white" size="25" />
+                  </a>
+                </div>
+                <div className="col-1">
+                  <a
+                    href="https://www.instagram.com/QuillonCurrency"
+                    target="blank"
+                  >
+                    <FaInstagram color="white" size="25" />
+                  </a>
+                </div>
+                <div className="col-1">
+                  <a href="https://twitter.com/QuillonCurrency" target="blank">
+                    <FaTwitter color="white" size="25" />
+                  </a>
+                </div>
+                <div className="col-1">
+                  <a href="https://github.com/QuillonCurrency" target="blank">
+                    <FaGithub color="white" size="25" />
+                  </a>
+                </div>
+                <div className="col-1">
+                  <a
+                    href="https://www.linkedin.com/company/quillonfoundation/"
+                    target="blank"
+                  >
+                    <FaLinkedin color="white" size="25" />
+                  </a>
+                </div>
+                <div className="col-1">
+                  <a href="https://quilloncurrency.slack.com" target="blank">
+                    <FaSlack color="white" size="25" />
+                  </a>
+                </div>
+                <div className="col-1">
+                  <a href="https://medium.com/quillon" target="blank">
+                    <FaMedium color="white" size="25" />
+                  </a>
+                </div>
+              </div>
+            </div>
+            <div className="col-lg-4"> </div>
           </div>
         </div>
       </div>
